27fbe7af600ff0b0f22397eb90e3d9fc20e955e6
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-06-12 12:43:06 +0200

    mistake in median heuristic

--- models/svgd.py
+++ models/svgd.py
@@ -13,7 +13,7 @@ def compute_phi(log_prob_grad, inputs, h, h_per_dimension=False):
         if not h_per_dimension:
             pairwise_distance = torch.norm(inputs[:, None] - inputs, dim=2).pow(2)  # [n, n]
             median = torch.quantile(pairwise_distance[mask], q=0.5)
-            h = median / torch.log(torch.tensor(n + 1.)) + 1e-6
+            h = median / (2 * torch.log(torch.tensor(n + 1.))) + 1e-6
             K = torch.exp( - pairwise_distance / (2 * h))
 
         else: # Compute different bandwidths h_1, ..., h_d
@@ -22,7 +22,7 @@ def compute_phi(log_prob_grad, inputs, h, h_per_dimension=False):
             mask_flat = mask.view(n * n)
             sq_diff_flat = sq_diff.view(n * n, d)
             sq_diff_off_diag = sq_diff_flat[mask_flat, :]
-            h = torch.quantile(sq_diff_off_diag, q=0.5, dim=0) / torch.log(torch.tensor(n + 1.)) + 1e-6
+            h = torch.quantile(sq_diff_off_diag, q=0.5, dim=0) / (2 * d * torch.log(torch.tensor(n + 1.))) + 1e-6
 
             scaled_sq_diff = sq_diff / (2 * h[None, None, :])
             K = torch.exp(- scaled_sq_diff.sum(dim=2))
