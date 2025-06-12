b5beab193c11eb8de0d9df7b7a25ec5b9f0c84c7
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-06-12 11:57:37 +0200

    svgd implementation

--- models/ParticleCloud.py
+++ models/ParticleCloud.py
@@ -1,5 +1,7 @@
 import torch
+from torch.optim import Adam
 import matplotlib.pyplot as plt
+from svgd import compute_phi
 
 class GmmPrior:
     def __init__(self, centers, tau):
@@ -21,7 +23,7 @@ class GmmPrior:
                         * torch.exp(- 0.5 * sq_dists / (tau ** 2))
         prior = torch.mean(gaussian_eval, dim = 0)
         log_prior = prior.log()
-        return log_prior
+        return torch.relu(log_prior + 10**4) - 10**4    # Clamp to low value to avoid -inf
 
 class ParticleCloud():
     def __init__(self, model, init_prior,
@@ -42,51 +44,8 @@ class ParticleCloud():
         particles = self.model.inverse_reparam(self.particles.detach())
         return particles
 
-    def plot(self, data):
-        particles = self.get_particles().numpy()
-        #self.prior = GmmPrior(centers = self.get_particles(), tau = self.tau)
-
-        fig, ax = plt.subplots(figsize=(5, 3))
-
-        x = torch.linspace(-1.0, 1.0, 50)
-        y = torch.log(torch.linspace(1e-5, 1.1, 50))
-        X, Y = torch.meshgrid(x, y, indexing='ij')
-        result = torch.stack([X.reshape(-1), Y.reshape(-1)], dim=1)
-
-        #out = self.prior.log_prob(self.model.inverse_reparam(result))
-        out = self.log_posterior(result, data)
-        out = out.reshape(shape = (50, 50))
-        max = out.max().item()
-        levels = torch.linspace(max * 0.5, max, 20).numpy()
-        contour = ax.contourf(X, torch.exp(Y), out, levels=levels, cmap='viridis', extend='min')
-        fig.colorbar(contour)
-
-        fine_levels = torch.linspace(max * 0.98, max, 5)
-        ax.contour(X, torch.exp(Y), out, levels=fine_levels, colors='k', linewidths=0.3)
-
-        ax.scatter(particles[:, 0], particles[:, 1], marker = '+', c='red')
-
-        ax.set_xlabel(r"$\mu$")
-        ax.set_ylabel(r"$\sigma$")
-        return fig
-
-    def gmm(self, eval: torch.tensor):
-        new_eval = self.model.inverse_reparam(eval)
-        previous_particles = self.model.inverse_reparam(self.previous_particles)
-        tau = self.tau
-
-        mean_sq = previous_particles.pow(2).sum(dim=1, keepdim=True)
-        eval_sq = new_eval.pow(2).sum(dim=1, keepdim=True)
-        cross_term = previous_particles @ new_eval.T
-        sq_dists = mean_sq + eval_sq.T - 2 * cross_term
-        gaussian_eval = (2 * torch.pi * tau**2)**(- self.n_params / 2) \
-                        * torch.exp(- 0.5 * sq_dists / (tau ** 2))
-        prior = torch.mean(gaussian_eval, dim = 0)
-        log_prior = prior.log()
-        return log_prior
-
     def log_posterior(self, eval, new_data):
-        log_p = self.model.ll(eval, new_data) \
+        log_p = self.model.ll(eval, new_data) / len(new_data) \
             + self.prior.log_prob(self.model.inverse_reparam(eval))
         return log_p
 
@@ -99,5 +58,74 @@ class ParticleCloud():
         self.prior = GmmPrior(centers = self.get_particles(), tau = self.tau)
         return
 
-    def update_svgd(self, new_data, n_steps, lr):
-        return NotImplementedError
+    def update_prior(self):
+        self.prior = GmmPrior(centers = self.get_particles(), tau = self.tau)
+
+    def update_svgd(self, new_data, n_steps, lr, h):
+        for i in range(n_steps):
+            optimizer = Adam([self.particles], lr=lr)
+            optimizer.zero_grad()
+
+            # Construct G
+            log_p = self.log_posterior(self.particles, new_data)
+            G = torch.autograd.grad(outputs=log_p.sum(), inputs=self.particles)[0]
+
+            with torch.no_grad():
+                phi = compute_phi(G, self.particles, h, h_per_dimension=False)
+            self.particles.grad = - phi
+            optimizer.step()
+        return
+
+    def plot_prior(self):
+        particles = self.get_particles()
+
+        fig, ax = plt.subplots(figsize=(5, 3))
+        p = 50
+        mus = torch.linspace(-1.0, 1.0, p)
+        sigmas = torch.linspace(0.01, 5.0, p)
+        mus, sigmas = torch.meshgrid(mus, sigmas, indexing='ij')
+        grid = torch.stack([mus.reshape(-1), sigmas.reshape(-1)], dim=1)
+
+        prior = self.prior.log_prob(grid).reshape(shape = (p, p))
+        max = prior.max().item()
+        levels = torch.linspace(-10.0, max, 20)
+        #if max < 0:
+        #    fine_levels = torch.linspace(max * 1.2, max, 5)
+        #else:
+        #    fine_levels = torch.linspace(max * 0.9, max, 5)
+        contour = ax.contourf(mus, torch.log(sigmas), prior, levels=levels, cmap='viridis', extend='min')
+        fig.colorbar(contour)
+        #ax.contour(mus, torch.log(sigmas), prior, levels=fine_levels, colors='k', linewidths=0.3)
+
+        ax.scatter(particles[:, 0], torch.log(particles[:, 1]), marker = '+', c='red', label='particles')
+
+        ax.set_xlabel(r"$\mu$")
+        ax.set_ylabel(r"$\log\sigma$")
+        return fig
+
+    def plot_posterior(self, new_data):
+        particles = self.get_particles()
+
+        fig, ax = plt.subplots(figsize=(5, 3))
+        p = 50
+        mus = torch.linspace(-1.0, 1.0, p)
+        log_sigmas = torch.log(torch.linspace(0.01, 5.0, p))
+        mus, log_sigmas = torch.meshgrid(mus, log_sigmas, indexing='ij')
+        grid = torch.stack([mus.reshape(-1), log_sigmas.reshape(-1)], dim=1)
+
+        posterior = self.log_posterior(grid, new_data).reshape(shape = (p, p))
+        max = posterior.max().item()
+        levels = torch.linspace(-10.0, max, 20)
+        #if max < 0:
+        #    fine_levels = torch.linspace(max * 1.2, max, 5)
+        #else:
+        #    fine_levels = torch.linspace(max * 0.98, max, 5)
+        contour = ax.contourf(mus, log_sigmas, posterior, levels=levels, cmap='viridis', extend='min')
+        fig.colorbar(contour)
+        #ax.contour(mus, log_sigmas, posterior, levels=fine_levels, colors='k', linewidths=0.3)
+
+        ax.scatter(particles[:, 0], torch.log(particles[:, 1]), marker = '+', c='red')
+
+        ax.set_xlabel(r"$\mu$")
+        ax.set_ylabel(r"$\log\sigma$")
+        return fig
