50397524eabe376417d489ab15506e864f378fc5
Nino Courtecuisse <ncourtecuisse@gmail.com>
2025-07-06 12:24:34 +0200

    Add NIG prior

--- utils/optimization.py
+++ utils/optimization.py
@@ -1,7 +1,7 @@
 import torch
 from torch import distributions as D
-from utils.priors import IndependentPrior, CevPrior
-from torch.distributions.transforms import ExpTransform
+from utils.priors import IndependentPrior, CevPrior, NigPrior
+import torch.distributions.transforms as T
 
 class IndependentTransform:
     def __init__(self, prior: IndependentPrior):
@@ -27,35 +27,57 @@ class CevTransform:
     def __init__(self, prior: CevPrior):
         self.mu_transform = D.transform_to(prior.mu_dist.support)
         self.beta_transform = D.transform_to(prior.beta_dist.support)
-        self.delta_transform = ExpTransform()  # Because LogNormal support is positive
+        self.delta_transform = T.ExpTransform()
 
         self.a = prior.a
         self.b = prior.b
 
     def to(self, unconstrained_x: torch.Tensor) -> torch.Tensor:
-        """
-        Maps unconstrained_x (in R^3) to constrained values (mu, delta, beta)
-        """
         u_mu, u_delta, u_beta = unconstrained_x[:, 0], unconstrained_x[:, 1], unconstrained_x[:, 2]
-
         c_mu = self.mu_transform(u_mu)
         c_beta = self.beta_transform(u_beta)
-
-        # LogNormal: delta = exp(u_delta) * exp(loc), i.e., exp(u_delta + a + b * beta)
         c_delta = self.delta_transform(u_delta + self.a + self.b * c_beta)
-
         return torch.stack([c_mu, c_delta, c_beta], dim=-1)
 
     def inv(self, constrained_x: torch.Tensor) -> torch.Tensor:
-        """
-        Maps constrained values (mu, delta, beta) to unconstrained_x in R^3
-        """
         c_mu, c_delta, c_beta = constrained_x[:, 0], constrained_x[:, 1], constrained_x[:, 2]
-
         u_mu = self.mu_transform.inv(c_mu)
         u_beta = self.beta_transform.inv(c_beta)
-
-        # Invert: u_delta = log(delta) - (a + b * beta)
         u_delta = self.delta_transform.inv(c_delta) - (self.a + self.b * c_beta)
-
         return torch.stack([u_mu, u_delta, u_beta], dim=-1)
+
+class NigTransform:
+    def __init__(self, prior: NigPrior):
+        self.mu_transform = D.transform_to(prior.mu_dist.support)
+        self.sigma_transform = D.transform_to(prior.sigma_dist.support)
+        self.gamma1_transform = D.transform_to(prior.gamma1_dist.support)
+        self.eps = 1e-10  # to enforce gamma2 > gamma1 strictly
+        self.gamma2_high = prior.gamma2_high
+
+    def _gamma2_transform(self, gamma1: torch.Tensor) -> T.ComposeTransform:
+        low = 5 * gamma1**2 / 3 + self.eps
+        high = self.gamma2_high
+        return T.ComposeTransform([
+            T.SigmoidTransform(),      # R -> (0, 1)
+            T.AffineTransform(loc=low, scale=high - low, event_dim=0)   # (0, 1) -> (low, high)
+        ])
+
+    def to(self, unconstrained_x: torch.Tensor) -> torch.Tensor:
+        u_mu, u_sigma, u_gamma1, u_gamma2 = unconstrained_x.T
+
+        mu = self.mu_transform(u_mu)
+        sigma = self.sigma_transform(u_sigma)
+        gamma1 = self.gamma1_transform(u_gamma1)
+        gamma2_transform = self._gamma2_transform(gamma1)
+        gamma2 = gamma2_transform(u_gamma2)
+        return torch.stack([mu, sigma, gamma1, gamma2], dim=-1)
+
+    def inv(self, constrained_x: torch.Tensor) -> torch.Tensor:
+        mu, sigma, gamma1, gamma2 = constrained_x.T
+
+        u_mu = self.mu_transform.inv(mu)
+        u_sigma = self.sigma_transform.inv(sigma)
+        u_gamma1 = self.gamma1_transform.inv(gamma1)
+        gamma2_transform = self._gamma2_transform(gamma1)
+        u_gamma2 = gamma2_transform.inv(gamma2)  
+        return torch.stack([u_mu, u_sigma, u_gamma1, u_gamma2], dim=-1)
--- utils/priors.py
+++ utils/priors.py
@@ -1,6 +1,7 @@
 import torch
 from torch import distributions as D
 from torchkde import KernelDensity
+from utils.distributions import ScaledBeta
 
 class IndependentPrior:
     def __init__(self, dists: list[D.Distribution]):
@@ -28,16 +29,12 @@ class IndependentPrior:
         return torch.stack(log_probs, dim=-1).sum(dim=-1)
 
 class CevPrior:
-    def __init__(self, mu_dist, beta_dist, delta_given_beta_fn=None, v=0.2, S=1000):
+    def __init__(self, mu_dist, beta_dist, v=0.2, S=1000):
         self.mu_dist = mu_dist
         self.beta_dist = beta_dist
         self.a = torch.tensor(v * S).log()
         self.b = -torch.tensor(S).log()/2
-
-        if delta_given_beta_fn is None:
-            self.delta_given_beta_fn = self._default_delta_given_beta
-        else:
-            self.delta_given_beta_fn = delta_given_beta_fn
+        self.delta_given_beta_fn = self._default_delta_given_beta
 
     def _default_delta_given_beta(self, beta):
         loc = self.a + self.b * beta
@@ -60,6 +57,35 @@ class CevPrior:
         logp_delta_given_beta = self.delta_given_beta_fn(beta).log_prob(delta)
 
         return logp_mu + logp_delta_given_beta + logp_beta
+
+class NigPrior:
+    def __init__(self, mu_dist, sigma_dist, gamma1_dist, gamma2_high=0.5):
+        self.mu_dist = mu_dist
+        self.sigma_dist = sigma_dist
+        self.gamma1_dist = gamma1_dist
+        self.gamma2_given_gamma1_fn = self._default_gamma2_given_gamma1
+        self.gamma2_high = gamma2_high
+
+    def _default_gamma2_given_gamma1(self, gamma1):
+        return ScaledBeta(2.0, 2.0, low=5 * gamma1**2 / 3, high=self.gamma2_high)
+
+    def sample(self, n_samples=1):
+        mu = self.mu_dist.sample((n_samples,))
+        sigma = self.sigma_dist.sample((n_samples,))
+        gamma1 = self.gamma1_dist.sample((n_samples,))
+        gamma2 = self.gamma2_given_gamma1_fn(gamma1).sample((n_samples, ))
+        samples = torch.stack([mu, sigma, gamma1, gamma2], dim=-1)
+        if n_samples == 1:
+            return samples
+        return samples.squeeze()
+
+    def log_prob(self, x: torch.Tensor) -> torch.Tensor:
+        mu, sigma, gamma1, gamma2 = x.T
+        logp_mu = self.mu_dist.log_prob(mu)
+        logp_sigma = self.sigma_dist.log_prob(sigma)
+        logp_gamma1 = self.gamma1_dist.log_prob(gamma1)
+        logp_gamma2 = self.gamma2_given_gamma1_fn(gamma1).log_prob(gamma2)
+        return logp_mu + logp_sigma + logp_gamma1 + logp_gamma2
 
 class KdePrior(KernelDensity):
     def __init__(self, particles, bandwidth):
